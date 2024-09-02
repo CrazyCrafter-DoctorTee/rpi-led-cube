a66cf51d714f47417e7ff48a1ca94ded5f0eb6b8
Travis <crazycrafter1@gmail.com>
2024-09-02 05:21:34 +0100

    Add clean exit from sigint

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod cube;
 
 use std::{
     iter::{once, repeat},
+    sync::Arc,
+    sync::atomic::{AtomicBool, Ordering},
     sync::mpsc::{sync_channel, Receiver, SyncSender, TryRecvError},
     thread::{self, JoinHandle},
     time::Duration,
@@ -24,7 +26,7 @@ struct Cli {
 }
 
 #[derive(Copy, Clone, ValueEnum)]
-enum Layer {
+enum Index {
     Zero,
     One,
     Two,
@@ -35,17 +37,17 @@ enum Layer {
     Seven,
 }
 
-impl From<Layer> for u8 {
-    fn from(item: Layer) -> Self {
+impl From<Index> for u8 {
+    fn from(item: Index) -> Self {
         match item {
-            Layer::Zero => 0,
-            Layer::One => 1,
-            Layer::Two => 2,
-            Layer::Three => 3,
-            Layer::Four => 4,
-            Layer::Five => 5,
-            Layer::Six => 6,
-            Layer::Seven => 7,
+            Index::Zero => 0,
+            Index::One => 1,
+            Index::Two => 2,
+            Index::Three => 3,
+            Index::Four => 4,
+            Index::Five => 5,
+            Index::Six => 6,
+            Index::Seven => 7,
         }
     }
 }
@@ -57,7 +59,9 @@ enum Program {
     /// Cycle one layer at a time
     Cycle,
     /// Turn on one full layer of LEDs
-    OneLayer { which: Layer },
+    OneLayer { which: Index },
+    /// Turn on one full row of LEDs
+    OneRow { which: Index },
 }
 
 fn spawn_display() -> (SyncSender<Frame>, JoinHandle<rppal::gpio::Result<()>>) {
@@ -83,15 +87,33 @@ fn spawn_display() -> (SyncSender<Frame>, JoinHandle<rppal::gpio::Result<()>>) {
     (tx, handler)
 }
 
-fn test_dummy_on() {
+fn test_dummy_on(stop_token: Arc<AtomicBool>) {
     let mut driver = CubeDriver::try_new().unwrap();
 
-    loop {
+    while !stop_token.load(Ordering::Relaxed) {
         driver.write_frame([[255; 8]; 8]);
     }
 }
 
-fn test_one_layer(layer: Layer) {
+fn test_one_row(row: Index, stop_token: Arc<AtomicBool>) {
+    let mut driver = CubeDriver::try_new().unwrap();
+
+    let layer_pattern: [u8; 8] = core::array::from_fn(|i| {
+        if i == u8::from(row).into() {
+            255
+        } else {
+            0
+        }
+    });
+
+    let frame = [layer_pattern; 8];
+
+    while !stop_token.load(Ordering::Relaxed) {
+        driver.write_frame(frame);
+    }
+}
+
+fn test_one_layer(layer: Index, stop_token: Arc<AtomicBool>) {
     let mut driver = CubeDriver::try_new().unwrap();
 
     let frame: [[u8; 8]; 8] = core::array::from_fn(|i| {
@@ -102,25 +124,25 @@ fn test_one_layer(layer: Layer) {
         }
     });
 
-    loop {
+    while !stop_token.load(Ordering::Relaxed) {
         driver.write_frame(frame);
     }
 }
 
-fn test_all_on() {
+fn test_all_on(stop_token: Arc<AtomicBool>) {
     let (sender, _handle) = spawn_display();
 
-    loop {
+    while !stop_token.load(Ordering::Relaxed) {
         sender.send([[255; 8]; 8]);
     }
 }
 
-fn test_cycle_layers() {
+fn test_cycle_layers(stop_token: Arc<AtomicBool>) {
     let (sender, _handle) = spawn_display();
 
     let mut layer_cycle = once([255; 8]).chain(repeat([0; 8]).take(8)).cycle();
 
-    loop {
+    while !stop_token.load(Ordering::Relaxed) {
         // Cycle through a window of 9 layers with one lit
         sender.send([
             layer_cycle.next().unwrap(),
@@ -138,9 +160,18 @@ fn test_cycle_layers() {
 fn main() {
     let args = Cli::parse();
 
+    let stop_token = Arc::new(AtomicBool::new(false));
+    let stop_token_clone = stop_token.clone();
+
+    ctrlc::set_handler(move || {
+        println!("Exiting...");
+        stop_token_clone.store(true, Ordering::Relaxed);
+    }).expect("Error setting Ctrl-C handler");
+
     match args.program {
-        Program::AllOn => test_dummy_on(),
-        Program::Cycle => test_cycle_layers(),
-        Program::OneLayer { which: layer } => test_one_layer(layer),
+        Program::AllOn => test_dummy_on(stop_token),
+        Program::Cycle => test_cycle_layers(stop_token),
+        Program::OneLayer { which: layer } => test_one_layer(layer, stop_token),
+        Program::OneRow { which: row } => test_one_row(row, stop_token),
     };
 }
